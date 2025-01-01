5a90b7465f411e975c8551927adf113fd184484a
Tuncayarda <tncyard@gmail.com>
2025-01-02 01:52:26 +0300

    servers and clients in python

--- build/my_py_pkg/build/lib/my_py_pkg/number_publisher.py
+++ build/my_py_pkg/build/lib/my_py_pkg/number_publisher.py
@@ -7,7 +7,7 @@ class NumberPublisher(Node):
     def __init__(self):
         super().__init__('number_publisher')
         self.publisher_ = self.create_publisher(Int64, '/number', 10)
-        self.number_ = 0
+        self.number_ = 0 
         self.timer_ = self.create_timer(1, self.publish)
         
     def publish(self):
